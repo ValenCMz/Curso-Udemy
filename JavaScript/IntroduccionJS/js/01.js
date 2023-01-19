d7a7a4ba465da90c2f9bf772b62e8d5a080c7c0b
Valentin <valentincaminos12345@gmail.com>
2023-01-19 12:42:09 -0300

    variables en js

---
+++ JavaScript/IntroduccionJS/js/01.js
@@ -0,0 +1,15 @@
+// Variables
+// Existen 2 formas de crear variables ya que "var" ya no se utiliza
+//los nombres de las variables no pueden iniciar con numero, tampoco guiones "-" 
+
+var producto = 'Audifonos gamer';//inicie la variable y le di valor
+
+var disponible;//inicio la variable sin valor
+
+producto = true; //como el lenguaje es de tipo dinamico, puedo reasignarle el valor a la variable
+
+var producto1 = 'Computadora', disponible1 = true, categoria = 'Computadoras'; //puedo inicializar 3 variables
+
+
+
+
