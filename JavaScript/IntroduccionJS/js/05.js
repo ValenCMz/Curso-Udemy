cfb59d4f77577a43fb9f7cc614430c36e91b4ab0
Valentin <valentincaminos12345@gmail.com>
2023-01-20 13:34:24 -0300

    numeros

---
+++ JavaScript/IntroduccionJS/js/05.js
@@ -0,0 +1,7 @@
+//Numeros
+//no existen los datos float, todos son number
+
+const numero1 = 100;
+const numero2 = 200;
+const numero3 = 200.20;
+console.log(numero3);//en la consola aparece de color violeta
