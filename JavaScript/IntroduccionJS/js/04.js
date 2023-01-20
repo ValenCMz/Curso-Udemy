c9e4251c1525cbbe1206c2629ff3a8ec528cb550
Valentin <valentincaminos12345@gmail.com>
2023-01-20 13:11:42 -0300

    creacion de strings

---
+++ JavaScript/IntroduccionJS/js/04.js
@@ -0,0 +1,4 @@
+const producto = "Monitor de 20 pulgadas"; //String (mas utilizada)
+const producto2 = String("30 pulgadas"); //Creamos la variable producto2 de tipo String a travez de un constructor
+const producto3 = new String("50 pulgadas"); //Creamos un objeto de tipo String
+
