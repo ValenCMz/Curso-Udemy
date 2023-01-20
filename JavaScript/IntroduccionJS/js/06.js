d3c5ccf072a40708ef731c32103d4c6d4498ce1d
Valentin <valentincaminos12345@gmail.com>
2023-01-20 14:19:46 -0300

    Objeto Math

---
+++ JavaScript/IntroduccionJS/js/06.js
@@ -0,0 +1,15 @@
+//Objeto Math
+
+let resultado;
+
+resultado = Math.PI;//nos da pi
+resultado = Math.round(2.3); //redondeamos
+resultado = Math.ceil(2.3);//siempre redondea hacia arriba
+resultado = Math.floor(2.3); //siempre redondea hacia abajo
+resultado = Math.sqrt(144); // raiz cuadrada
+resultado = Math.abs(-200); //convierte a positivo
+resultado = Math.min(3,5,2,8,10);//te da el numero menos
+resultado = Math.max(3,5,2,8,10);//te da el numero mayor
+resultado = Math.random();//numero aleatorio
+
+console.log(resultado);
