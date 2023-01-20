2ebee509f1b6818ade19ddcf6719ff1b15a54cf0
Valentin <valentincaminos12345@gmail.com>
2023-01-20 12:37:42 -0300

    uso de const y let

---
+++ JavaScript/IntroduccionJS/js/02.js
@@ -0,0 +1,15 @@
+// Variables
+// Existen 2 formas de crear variables ya que "var" ya no se utiliza
+// los nombres de las variables no pueden iniciar con numero, tampoco guiones "-" 
+// las variables con let se utilizan igual
+let producto = 'Audifonos gamer';//inicie la variable y le di valor
+
+let disponible;//inicio la variable sin valor
+
+producto = true; //como el lenguaje es de tipo dinamico, puedo reasignarle el valor a la variable
+
+let producto1 = 'Computadora', disponible1 = true, categoria = 'Computadoras'; //puedo inicializar 3 variables
+
+
+
+
---
+++ JavaScript/IntroduccionJS/js/03.js
@@ -0,0 +1,14 @@
+// Variables con const
+// Si vas a tener un valor fijo q no se va a modificar, se utiliza const
+
+const producto = 'Audifonos gamer';//inicie la variable y le di valor
+
+//const disponible; //no se puede inicializar una varibale sin valor
+
+producto = true; //no puedo reasignarle un valor a una constante
+
+const producto1 = 'Computadora', disponible1 = true, categoria = 'Computadoras'; //puedo inicializar 3 variables
+
+
+
+
