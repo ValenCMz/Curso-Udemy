e7905f885a440fc096066a199a53a274a455dd70
Valentin <valentincaminos12345@gmail.com>
2023-01-24 14:36:21 -0300

    concatenaciones y templates

---
+++ JavaScript/IntroduccionJS/js/08.js
@@ -0,0 +1,9 @@
+const nombre = 'valen';
+const email = 'valen@hotmail.com';
+
+//concatenacion
+console.log(nombre + email) //valenvalen@hotmail.com
+console.log(nombre + " " + email);//valen valen@hotmail.com
+
+//Templates Strings (Mejor sintaxis)
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`);//Nombre Cliente: valen Email: valen@hotmail.com
